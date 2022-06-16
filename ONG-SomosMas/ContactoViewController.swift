612c5aec571a310a5b132fed8e45473490f5586e
cosmiqu3 <107438895+cosmiqu3@users.noreply.github.com>
2022-06-16 16:26:23 -0400

    Se crea Pantalla Registro y LaunchScreen

--- ONG-SomosMas/ContactoViewController.swift
+++ ONG-SomosMas/ContactoViewController.swift
@@ -15,15 +15,35 @@ class ContactoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var mensageTextField: UITextField!{
+        didSet{
+            var frame: CGRect = mensageTextField.frame
+                frame.size.height = 100
+            mensageTextField.frame = frame
+            
+        }
     }
-    */
+    
+    
+    @IBOutlet weak var donarButton: UIButton!{
+        didSet{
+            donarButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+            donarButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+            donarButton.layer.shadowOpacity = 1.0
+            donarButton.layer.shadowRadius = 2.0
+            donarButton.layer.masksToBounds = false
+        }
+    }
+    
+    
+    @IBOutlet weak var enviarButton: UIButton!{
+        didSet{
+            enviarButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+            enviarButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+            enviarButton.layer.shadowOpacity = 1.0
+            enviarButton.layer.shadowRadius = 2.0
+            enviarButton.layer.masksToBounds = false
+        }
+    }
 
 }
