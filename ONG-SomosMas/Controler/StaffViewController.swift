3b6bb95ca7041bb9dc5246a57fd78240d7688a7f
jpradon <106857945+jpradon@users.noreply.github.com>
2022-06-18 17:29:25 -0400

    adición de formularios de testimonios y staff

---
+++ ONG-SomosMas/Controler/StaffViewController.swift
@@ -0,0 +1,38 @@
+//
+//  StaffViewController.swift
+//  ONG-SomosMas
+//
+//  Created by training on 18-06-22.
+//
+
+import UIKit
+
+class StaffViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var rolLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
