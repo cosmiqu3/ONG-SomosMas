f68f67726f29038d41a1e2800dc612cf79c34276
jpradon <106857945+jpradon@users.noreply.github.com>
2022-06-18 11:46:46 -0400

    cambios

---
+++ ONG-SomosMas/Model/Testimonial.swift
@@ -0,0 +1,22 @@
+//
+//  Testimonial.swift
+//  ONG-SomosMas
+//
+//  Created by training on 18-06-22.
+//
+
+import Foundation
+
+struct Testimonial: Hashable {
+    
+    var name: String
+    var description: String
+    var photo: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    static func ==(lhs: Testimonial, rhs: Testimonial) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
