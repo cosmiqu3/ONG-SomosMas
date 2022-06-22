7a63ab33c720680ad362404bf851e39377211d6a
cosmiqu3 <107438895+cosmiqu3@users.noreply.github.com>
2022-06-22 19:42:20 -0400

    Se actualiza pantalla Login

--- ONG-SomosMas/Controler/LoginViewController.swift
+++ ONG-SomosMas/Controler/LoginViewController.swift
@@ -9,21 +9,110 @@ import UIKit
 
 class LoginViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    
+    
+    
+    
+    
+    @IBAction func emailChanged(_ sender: Any) {
+        if let email = emailTextField.text {
+            
+            if let errorMenssage = invalidEmail(email) {
+                emailErrorLabel.text = errorMenssage
+                emailErrorLabel.isHidden = false
+                LoginButton.isEnabled = true
+            }else {
+                emailErrorLabel.isHidden = true
+                LoginButton.isEnabled = false
+            }
+        }
+        checForValidForm()
+    }
+    
+    
+    @IBAction func onTapLogin(_ sender: Any) {
+        
+        bloqueoButton()
+        
+    }
+    
+    
+    @IBAction func passChanged(_ sender: Any) {
+        
+        if let name = passwordTextField.text{
+            if let errorMessage = invalidoPass(name){
+                passwordErrorLabel.text = errorMessage
+                passwordErrorLabel.isHidden = false
+            }else {
+                passwordErrorLabel.isHidden = true
+            }
+        }
+        checForValidForm()
+    }
+    
+    func invalidoPass(_ value: String) ->String?{
 
-        // Do any additional setup after loading the view.
+        if value == "" {
+            return "Favor ingrese Contraseña"
+        }
+        return nil
+    }
+    
+ 
+    func bloqueoButton(){
+        
+//        LoginButton.isEnabled = false
+       
+        emailErrorLabel.isHidden = true
+        passwordErrorLabel.isHidden = true
+        
+        emailErrorLabel.text = "Requerido"
+        passwordErrorLabel.text = "Requerido"
+        
+        emailErrorLabel.text = ""
+        passwordErrorLabel.text = ""
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func checForValidForm(){
+        if emailErrorLabel.isHidden && passwordErrorLabel.isHidden {
+            LoginButton.isEnabled = true
+        }else {
+            LoginButton.isEnabled = false
+        }
     }
-    */
+    
+    func invalidEmail(_ value: String) -> String? {
+        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,20}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        if !predicate.evaluate(with: value) {
+            return "Email inválido"
+        }
+        return nil
+    }
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bloqueoButton()
+    }
+    
+
+   
+    
+    
+    
+    
+    
 
 }
